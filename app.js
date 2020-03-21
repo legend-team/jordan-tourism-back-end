15e09a97c746569923219c7e861babe876747378
SajaSwalgah <saja.swalgah@yahoo.com>
2020-03-21 14:45:15 +0200

    oo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 const server = require('./lib/server.js');
 
 
-const MONGOOSE_URI = process.env.MONGOOSE_URI;
+const MONGOOSE_URI = '//localhost:27017/joTourism';
 const mongoose_option = {
     useNewUrlParser:true,
     useCreateIndex:true,
